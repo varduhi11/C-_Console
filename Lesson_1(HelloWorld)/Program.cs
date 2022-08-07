c6614ab227697b6aab4464410f3d68908f1532da
Varduhi <55928140+varduhi11@users.noreply.github.com>
2022-08-07 12:03:38 +0400

    Hello, World! / 1. Задание

---
+++ Lesson_1(HelloWorld)/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+// 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе
+// показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+int x1 = 456 % 10;
+int x2 = 782 % 10;
+int x3 = 918 % 10;
+
+Console.WriteLine(x1 , x2 , x3)
